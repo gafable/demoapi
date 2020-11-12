62b1f155ed5eb5d32c97e6b274da3c0249689a3e
LoremZyn14 <yasuaimo@gmail.com>
2020-11-12 11:47:33 +0800

    add git ignore

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const port = 3000;
 const app = express();
 
-const database = require("./services/database");
-const MovieRouter = require("./routes/movies");
+const database = require("./services/dbConnection");
+const BookStoreRoutes = require("./routes/bookStoreRoutes");
 
 app.use(express.json());
-app.use("/dashboard", MovieRouter);
+
+app.use("/api/bookstore", BookStoreRoutes);
+
 database.connect();
 
-app.listen(8000, console.log("Server running on port 8000"));
+app.listen(port, console.log(`Server running on port ${port}`));
